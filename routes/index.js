ab4638a229c37bfd022cf0822480c98424f764a6
Deamon12 <deamondm@gmail.com>
2018-02-03 15:19:18 -0800

    Adjusted /googlereview route

--- routes/index.js
+++ routes/index.js
@@ -12,8 +12,9 @@ router.get('/', function(req, res, next) {
 router.get('/googlereviews', function (req, res) {
 
     //Return without exception if env is missing
-    if(process.env.GOOGLE_PLACE_ID1 === undefined || process.env.GOOGLE_PLACES_KEY === undefined){
-        res.json("error");
+    if(process.env.GOOGLE_PLACE_ID === undefined || process.env.GOOGLE_PLACES_KEY === undefined){
+        //res.json("error");
+        //return;
     }
     else {
         var url = "https://maps.googleapis.com/maps/api/place/details/json?placeid="+process.env.GOOGLE_PLACE_ID+"&key="+process.env.GOOGLE_PLACES_KEY;
