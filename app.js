21cf0074c5ff97a4eaada4268f6120fb70861747
Deamon12 <deamondm@gmail.com>
2018-05-19 08:54:58 -0700

    Fixed missing images. Hardcoded for now. Added grayscale image to juices/acces Adjusted some margins on footer

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 var dotenv = require('dotenv').config();
-
-//var logger = require('morgan');
-//var cookieParser = require('cookie-parser');
-//var bodyParser = require('body-parser');
+var passport = require('passport');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -19,10 +16,6 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-//app.use(logger('dev'));
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
@@ -45,5 +38,35 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error');
 });
+
+/*
+var InstagramStrategy = require('passport-instagram').Strategy;
+
+var INSTAGRAM_CLIENT_ID = '9999b9b4a7dd48c9848933df77eed962';
+var INSTAGRAM_CLIENT_SECRET = '9baced46c61547c9a65d4fbc3e06b027';
+
+passport.use(new InstagramStrategy({
+        clientID: INSTAGRAM_CLIENT_ID,
+        clientSecret: INSTAGRAM_CLIENT_SECRET,
+        callbackURL: "http://localhost:3030/auth/instagram/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+        // asynchronous verification, for effect...
+        process.nextTick(function () {
+
+            console.log("TEStING: "+accessToken);
+            // To keep the example simple, the user's Instagram profile is returned to
+            // represent the logged-in user.  In a typical application, you would want
+            // to associate the Instagram account with a user record in your database,
+            // and return that user instead.
+            return done(null, profile);
+        });
+    }
+));
+*/
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var request = require('request');
 var router = express.Router();
-
+var passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'GreatVape'});
+  res.render('index', { title: 'GreatVapeCo'});
 });
 
 
@@ -19,12 +19,61 @@ router.get('/googlereviews', function (req, res) {
     else {
         var url = "https://maps.googleapis.com/maps/api/place/details/json?placeid="+process.env.GOOGLE_PLACE_ID+"&key="+process.env.GOOGLE_PLACES_KEY;
         request.get({ url: url }, function(error, response, body) {
-            if (!error && response.statusCode == 200) {
+            if (!error && response.statusCode === 200) {
                 res.json(body);
             }
         });
     }
-
 });
+
+/*
+var InstagramTokenStrategy = require('passport-instagram-token');
+var InstagramStrategy = require('passport-instagram').Strategy;
+
+var INSTAGRAM_CLIENT_ID = '9999b9b4a7dd48c9848933df77eed962';
+var INSTAGRAM_CLIENT_SECRET = '9baced46c61547c9a65d4fbc3e06b027';
+*/
+/*
+passport.use(new InstagramStrategy({
+        clientID: INSTAGRAM_CLIENT_ID,
+        clientSecret: INSTAGRAM_CLIENT_SECRET,
+        callbackURL: "http://localhost:3000/auth/instagram/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+        // asynchronous verification, for effect...
+        process.nextTick(function () {
+
+            console.log("TEStING: "+accessToken);
+            // To keep the example simple, the user's Instagram profile is returned to
+            // represent the logged-in user.  In a typical application, you would want
+            // to associate the Instagram account with a user record in your database,
+            // and return that user instead.
+            return done(null, profile);
+        });
+    }
+));*/
+
+//app.use(passport.initialize());
+//app.use(passport.session());
+
+/*
+passport.use(new InstagramTokenStrategy({
+    clientID: INSTAGRAM_CLIENT_ID,
+    clientSecret: INSTAGRAM_CLIENT_SECRET,
+    passReqToCallback: true
+}, function(req, accessToken, refreshToken, profile, next) {
+    console.log("req: "+req);
+    console.log("Accesstoken: "+accessToken);
+
+}));
+*/
+/*
+const Instagram = require('node-instagram').default;
+// Create a new instance.
+const instagram = new Instagram({
+    clientId: '9999b9b4a7dd48c9848933df77eed962',
+    clientSecret: '9baced46c61547c9a65d4fbc3e06b027',
+    accessToken: 'user-access-token',
+});*/
 
 module.exports = router;
